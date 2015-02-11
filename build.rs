bb3cb3455b6185353a800253e461d6a35d977904
Alex Crichton <alex@alexcrichton.com>
2015-02-10 20:53:37 -0800

    Update gcc

--- build.rs
+++ build.rs
@@ -1,18 +1,6 @@
 extern crate gcc;
 
-use std::default::Default;
-
 fn main() {
-    gcc::compile_library("libsiphash.a",
-                         &gcc::Config {
-                             flags: vec!["-O3".to_string()],
-                             ..Default::default()
-                         },
-                         &["siphash24.c"]);
-    gcc::compile_library("libsiphash2.a",
-                         &gcc::Config {
-                             flags: vec!["-O3".to_string()],
-                             ..Default::default()
-                         },
-                         &["csiphash.c"]);
+    gcc::Config::new().file("siphash24.c").flag("-O3").compile("libsiphash.a");
+    gcc::Config::new().file("csiphash.c").flag("-O3").compile("libsiphash2.a");
 }
